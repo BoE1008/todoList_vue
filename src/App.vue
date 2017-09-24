80ec0b949ce095a16abc8386451d37643845408f
BoE1008 <15871807472@163.com>
2017-09-24 08:38:50 +0800

    todoList_VUE

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <component-a :sonmsg="msg" @childsend="showfromchild"></component-a>
+  </div>
+</template>
+
+<script>
+import componentA from './components/componentA';
+export default {
+  data(){
+    return{
+      msg:"父传子"
+    }
+  },
+  components: {
+    componentA
+  },
+  methods:{
+    showfromchild(data){
+      alert(data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/componentA.vue
@@ -0,0 +1,59 @@
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+a {
+  color: #42b983;
+}
+
+.finished {
+  text-decoration: underline;
+}
+</style>
+
+<template>
+<div>
+  <input type="text" v-model="newItem" v-on:keyup.enter="enter">
+  <ul>
+    <li v-for="item in todos" v-on:click="liClick(item)" v-bind:class="{finished:item.isFinished}">
+      {{item.label}}
+    </li>
+  </ul>
+  <div>
+    {{sonmsg}}
+  </div>
+  <button type="button" @click='tofather'>子传父</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '',
+      newItem:'',
+      todos: [],
+    }
+  },
+  props:['sonmsg'],
+  methods: {
+    liClick(item) {
+      item.isFinished = !item.isFinished;
+    },
+    enter(){
+      this.todos.push({label:this.newItem,isFinished:false});
+      this.newItem = '';
+    },
+    tofather(){
+      this.$emit("childsend","this is from child")
+    }
+  }
+}
+</script>
